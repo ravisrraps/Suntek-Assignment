7192b83c8d96a0ab1ba66c0e54f651f4abae275b
ravi-am <71038034+ravisrraps@users.noreply.github.com>
2021-02-21 22:52:31 +0530

    Add files via upload

---
+++ SearchinRotatedSortedArray.c
@@ -0,0 +1,10 @@
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        for i in range(len(nums)):
+            if(target in nums):
+                return(nums.index(target))
+                break
+            else:
+                return(-1)
+                break
+        
